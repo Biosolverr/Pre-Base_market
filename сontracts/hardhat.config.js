f1dbcf035bcfe8fec23d5fa374b0963eb601dcd2
Biosolver <asperant001@gmail.com>
2026-05-14 20:53:15 +0700

    Add files via upload

---
+++ сontracts/hardhat.config.js
@@ -0,0 +1,11 @@
+require("@nomicfoundation/hardhat-toolbox");
+
+module.exports = {
+  solidity: "0.8.24",
+  networks: {
+    base: {
+      url: "https://base-mainnet.g.alchemy.com/v2/xwWoX_wtM8OoPxNB3eLt5",
+      accounts: [],
+    }
+  }
+};
